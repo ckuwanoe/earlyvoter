ec6015a3faff14107447616aab137125756ecfd7
Corey Kuwanoe <corey.kuwanoe@gmail.com>
2012-06-29 11:07:02 -0500

    adding validations

--- app/models/voter.rb
+++ app/models/voter.rb
@@ -8,8 +8,33 @@ class Voter < ActiveRecord::Base
   # store the fetched address in the full_address attribute
   reverse_geocoded_by :lat, :lng, :address => :gmaps4rails_address
 
+  validates :voter_name, :presence => true
+  validates :street_address, :presence => true
+  validates :city, :presence => true
+  validates :state, :presence => true
+  validates :zipcode, :presence => true
+  validates :email, :presence => true
+  validates :phone, :presence => true
+
   def gmaps4rails_address
   #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
     "#{self.street_address}, #{self.city}, #{self.state}, #{self.zipcode}" 
+  end
+  
+  def gmaps4rails_marker_picture
+    {
+     "picture" => "http://media.roryreid.com.s3.amazonaws.com/house-image.png",
+     "width" => 32,
+     "height" => 32,
+     "marker_anchor" => [ 5, 10],
+     "shadow_picture" => "/images/morgan.png" ,
+     "shadow_width" => "110",
+     "shadow_height" => "110",
+     "shadow_anchor" => [5, 10],
+    }
+  end
+  
+  def gmaps4rails_infowindow
+      "You are here:<br>#{self.street_address}<br>#{self.city}, #{self.state} #{self.zipcode}"
   end
 end
